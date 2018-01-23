db0cf6e4d6cdc87d58da7a4fe4eb732d36bd67b2
owner <owner@DHU-1>
2018-01-23 18:59:12 +0900

    add date

--- Apple/src/HelloWorld.java
+++ Apple/src/HelloWorld.java
@@ -1,10 +1,12 @@
+import java.util.Date;
 
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("³»¿ë º¯°æµÊ by Team1");
-
+		Date date = new Date();
+		System.out.println(date.toString());
 	}
 
 }
