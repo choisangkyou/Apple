8facbcba4e692cc893ec2218409f0047c62ce407
owner <owner@DHU-1>
2018-02-27 20:09:07 +0900

    Thread names

---
+++ Apple/src/chap12/sec02_exam03_thread_name/ThreadNameExample.java
@@ -0,0 +1,28 @@
+package chap12.sec02_exam03_thread_name;
+
+public class ThreadNameExample {
+
+	public static void main(String[] args) {
+		Thread mainTread = Thread.currentThread();
+		System.out.println("프로그램 시작 스레드 이름:"+ mainTread.getName());
+
+
+	
+		ThreadA threadA = new ThreadA();
+		System.out.println("작업 스레드 이름:"+threadA.getName());
+		threadA.start();
+		
+		try {
+			Thread.sleep(500);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		ThreadB threadB = new ThreadB();
+		System.out.println("작업 스레드 이름:"+threadB.getName());
+		threadB.start();
+		
+
+	}
+
+}
